b653745f6d9a5d1bc5f186304632088a4be03a63
Fred Menge <mengeprofissional@gmail.com>
2024-07-04 14:59:26 -0300

    Criacao de funções necessárias

--- src/evaluation.js
+++ src/evaluation.js
@@ -1,1 +1,7 @@
 "use strict";
+const multiply = (number1, number2) => { return number1 * number2; };
+const helloName = (name) => {
+    return ('Hello ' + name);
+};
+console.log(helloName('Fred'));
+console.log(multiply(5, 9));
